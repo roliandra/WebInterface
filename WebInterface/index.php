a2ed37999e232990f57bf7eafc5b269ce3393da2
roliandra <roli@uni-help.net>
2012-01-31 01:35:05 +0100

    localization fix

--- WebInterface/index.php
+++ WebInterface/index.php
@@ -38,6 +38,9 @@
     <script type="text/javascript" language="javascript" src="js/inputfunc.js"></script>
     <script type="text/javascript" charset="utf-8">
       $(document).ready(function() {
+        "oLanguage": {
+              "sUrl": "languages/datatables.<?php echo $language; ?>.txt"
+        },
         oTable = $('#mainTable').dataTable({
           "bProcessing"     : true,
           "bJQueryUI"       : true,
